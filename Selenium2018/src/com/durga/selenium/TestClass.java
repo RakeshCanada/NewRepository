8c44a172a8fb70f14c6d952086f479ba49892d3b
RakeshCanada <rakesh1.chauhan@gmail.com>
2018-08-26 22:31:39 -0400

    Add new repository Selenium 2018
    Add new repository Selenium 2018

---
+++ Selenium2018/src/com/durga/selenium/TestClass.java
@@ -0,0 +1,12 @@
+package com.durga.selenium;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is test class");
+
+	}
+
+}
