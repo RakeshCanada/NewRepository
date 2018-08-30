275e088682a96b6e9f946956c639023fe1212b4f
RakeshCanada <rakesh1.chauhan@gmail.com>
2018-08-29 23:04:34 -0400

    Added new class on Aug 30

---
+++ Selenium2018/src/com/durga/selenium/TestAug30.java
@@ -0,0 +1,10 @@
+package com.durga.selenium;
+
+public class TestAug30 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test class");
+	}
+
+}
